8e83bd101d77f20f8a28a4d79f334a921ee221a0
Evgen93 <36450112+Evgen93@users.noreply.github.com>
2018-09-04 21:25:52 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+
+namespace task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] digits = new string[9]
+        {
+            "Сто",
+            "Двести",
+            "Триста",
+            "Четыреста",
+            "Пятьсот",
+            "Шестьсот",
+            "Cемьсот",
+            "Восемьсот",
+            "Девятсот"
+        };
+            Console.WriteLine("Введите число от 100 до 999");
+            string strA = Console.ReadLine();
+            int numb = Int32.Parse(strA);
+            string representation = numb.ToString();
+            int tmp = numb / 100;
+            Console.WriteLine("{0}", numb/100);
+            for (int i = 0; i < 5; i++)
+            {
+                if(tmp == i+1)
+                {
+                    Console.WriteLine("{0}", digits[i]);
+                }
+            }
+            Console.Read();
+        }
+    }
+}
